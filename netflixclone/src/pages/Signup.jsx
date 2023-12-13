72157bc4a6389c6736a9bc37b1017dca86087abf
Ahmad Dhami <dhami451@gmail.com>
2023-12-13 11:46:13 +0500

    Refactor CSS and add login and signup functionality

--- netflixclone/src/pages/Signup.jsx
+++ netflixclone/src/pages/Signup.jsx
@@ -1,42 +1,84 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Header from '../components/Header'
-import BackgroundImage from '../components/BackgroundImage'
-export default function Signup() {
-  const [showPassword,setShowPassword] = useState(false)
-  const [formValues,setformValues]=useState({
-    email:'',
-    password:''
-  })
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
+import { firebaseAuth } from "../utils/firebase-config";
+function Signup() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSignIn = async () => {
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
+
   return (
     <Container showPassword={showPassword}>
-      <BackgroundImage/>
+      <BackgroundImage />
       <div className="content">
-        <Header/>
-        <div className='body flex column a-center j-center'>
-            <div className='text flex column'>
-                <h1>Unlimited movies,TV shows and more</h1>
-                <h4>Watch anywhere.Cancel anytime.</h4>
-                <h5>Ready to watch? Enter your email to create or restart your membership.</h5>
-            </div>
-            <div>
-                <input type='email' placeholder='Email Address' name='email'
-                  value={formValues.email} onChange={(e)=>setformValues({...formValues,[e.target.name]: e.target.value})}/>
-                {showPassword && (
-                  <input type='password' placeholder='Password' name='password'/>
-                )}
-                {!showPassword &&(
-                  <button onclick={()=>setShowPassword(true)}>Get Started</button>
-
-                )}
-            </div>
-            <button>Log In</button>
+        <Header login />
+        <div className="body flex column a-center j-center">
+          <div className="text flex column">
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <h4>Watch anywhere. Cancel anytime.</h4>
+            <h6>
+              Ready to watch? Enter your email to create or restart membership.
+            </h6>
+          </div>
+          <div className="form">
+            <input
+              type="email"
+              placeholder="Email address"
+              onChange={(e) =>
+                setFormValues({
+                  ...formValues,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              name="email"
+              value={formValues.email}
+            />
+            {showPassword && (
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  setFormValues({
+                    ...formValues,
+                    [e.target.name]: e.target.value,
+                  })
+                }
+                name="password"
+                value={formValues.password}
+              />
+            )}
+            {!showPassword && (
+              <button onClick={() => setShowPassword(true)}>Get Started</button>
+            )}
+          </div>
+          {showPassword && <button onClick={handleSignIn}>Sign Up</button>}
         </div>
       </div>
-    </Container> 
-    )
+    </Container>
+  );
 }
-
 
 const Container = styled.div`
   position: relative;
@@ -97,3 +139,5 @@ const Container = styled.div`
     }
   }
 `;
+
+export default Signup;
